d6f772de41c18634389a963b200588983b1684f9
qiwen.cheng <qiwen.cheng@kangcenet.com>
2018-09-05 09:09:38 +0800

    更新

--- src/main/java/com/unual/anime/web/AnimeController.java
+++ src/main/java/com/unual/anime/web/AnimeController.java
@@ -3,17 +3,13 @@ package com.unual.anime.web;
 import com.unual.anime.dto.Result;
 import com.unual.anime.entity.Anime;
 import com.unual.anime.entity.AnimeVideo;
-import com.unual.anime.entity.Money;
 import com.unual.anime.service.AnimeService;
 import com.unual.anime.service.AnimeVideoService;
-import com.unual.anime.service.TestService;
 import org.apache.ibatis.annotations.Options;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.interceptor.TransactionAspectSupport;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -25,10 +21,6 @@ import static com.unual.anime.utils.ConvertUtils.*;
 @Controller
 @RequestMapping("/")
 public class AnimeController {
-
-    @Autowired
-    private TestService testService;
-
     @Autowired
     private AnimeService animeService;
 
@@ -83,45 +75,4 @@ public class AnimeController {
         List<AnimeVideo> animeList = animeVideoService.getVideoList(id, page, limit);
         return animeList;
     }
-
-    @ResponseBody
-    @RequestMapping(value = "test/money/{id}", method = RequestMethod.GET)
-    private Result<Integer> getMoneyById(@PathVariable("id") int id) {
-        Money money = testService.getMoneyById(id);
-        Result<Integer> result = new Result<>(money.getMoney());
-        return result;
-    }
-
-    static int a = 1;
-
-
-    @ResponseBody
-    @Options(timeout = 10000, flushCache = true)
-    @RequestMapping(value = "test/money/{id}/subtract", method = RequestMethod.GET)
-    @Transactional(rollbackFor = Exception.class)
-    public Result<Boolean> updateMoneyById(@PathVariable("id") int id, @RequestParam("money") int money) throws Exception {
-        Money origin = testService.getMoneyById(id);
-        Result<Boolean> result = null;
-        if (origin.getMoney() - money > 0) {
-            System.err.println("" + origin.getMoney() + " - " + money);
-            Thread.sleep(1000);
-            testService.insertMoneyRecordById(id, "-" + money);
-            int a = testService.updateMoneyById(id, origin.getMoney() - money, origin.getVersion());
-            System.err.println("" + origin.getMoney() + " - " + money + " - " + a);
-            if (a <= 0) {
-                throw new Exception("请勿频繁操作!");
-            }
-            result = new Result<Boolean>(true);
-        } else {
-            result = new Result<Boolean>("金额不够！");
-        }
-        return result;
-    }
-
-    @Transactional(propagation = Propagation.REQUIRES_NEW, rollbackFor = Exception.class)
-    public boolean checkMoney(int origin, int id) {
-
-        return true;
-    }
-
 }
